4797aebd6a7052610acc73367e4238f9c3b04b6c
Atreya Jariwala <aajariwala2007@gmail.com>
2026-09-03 19:37:54 -0500

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,22 @@
+class TimeMap:
+
+    def __init__(self):
+        self.mapp = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.mapp[key].append([timestamp,value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        vals = self.mapp[key]
+        if not vals: return ""
+        res = ""
+        i = 0
+        j = len(vals) - 1
+        while i <= j:
+            mid = (i+j) // 2
+            if timestamp >= vals[mid][0]:
+                res = vals[mid][1]
+                i = mid + 1
+            else:
+                j = mid - 1
+        return res
